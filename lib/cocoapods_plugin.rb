61a1e0d96252e6f76423edde601cd2a7ed9da023
Samuel Giddins <segiddins@segiddins.me>
2018-04-02 09:45:26 -0400

    Rewrite the plugin to monkey-patch cocoapods installer methods  (#3)
    * Add specs
    
    * Rewrite the plugin to monkey-patch cocoapods installer methods
    
    This allows us to avoid needing to re-write the user project on every single pod install, which will save multiple seconds
    
    * Fix rubocop violations
    
    * Manually install bundler on Travis

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -1,7 +1,3 @@
 # frozen_string_literal: true
 
 require 'cocoapods_amicable'
-
-Pod::HooksManager.register 'cocoapods-amicable', :post_install do |post_install_context|
-  CocoaPodsAmicable::PodfileChecksumFixer.new(post_install_context).fix!
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,44 @@
 # frozen_string_literal: true
 
 require 'cocoapods'
+
+module Pod
+  # Disable the wrapping so the output is deterministic in the tests.
+  #
+  UI.disable_wrap = true
+
+  # Redirects the messages to an internal store.
+  #
+  module UI
+    class << self
+      attr_accessor :output
+      attr_accessor :warnings
+      attr_accessor :next_input
+
+      def puts(message = '')
+        @output << "#{message}\n"
+      end
+
+      def warn(message = '', _actions = [])
+        @warnings << "#{message}\n"
+      end
+
+      def print(message)
+        @output << message
+      end
+
+      alias gets next_input
+
+      def print_warnings; end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.before(:each) do
+    Pod::UI.output = ''.dup
+    Pod::UI.warnings = ''.dup
+    Pod::UI.next_input = ''.dup
+    Pod::Config.instance = nil
+  end
+end
